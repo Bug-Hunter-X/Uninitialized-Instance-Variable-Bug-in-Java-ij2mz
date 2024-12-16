f82ca80338b4907839a04ed8d7876be9bf3bef7f
BugHunter-X <askzenai@gmail.com>
2024-12-16 07:36:05 +0100

    Add Bug.java file

---
+++ Bug.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    private int x;
+
+    public MyClass(int x) {
+        this.x = x; // Assign value to the instance variable
+    }
+
+    public int getX() {
+        return x; // Return the value of the instance variable
+    }
+}
